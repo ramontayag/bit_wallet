028f978610405242181187e3f1775fd531ecd0ed
Ramon Tayag <ramon@tayag.net>
2013-03-03 17:29:40 +0800

    Use bitcoin_testnet for managing the testnet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
 APP_ENV = 'test'
-Bundler.require(APP_ENV)
+Bundler.require(:default, APP_ENV)
 
 require 'bit_wallet'
 require 'ostruct'
@@ -9,6 +9,7 @@ require 'active_support/core_ext/hash/slice'
 require 'pry'
 require 'factory_girl'
 require_relative 'factories'
+require 'bitcoin_testnet'
 
 Config = {}
 yaml_config_file = File.open(File.join(File.dirname(__FILE__), 'config.yml'))
@@ -16,22 +17,14 @@ yaml_config = YAML.load(yaml_config_file).with_indifferent_access
 Config[:username] = yaml_config[:rpcuser]
 Config[:port] = yaml_config[:rpcport]
 Config[:password] = yaml_config[:rpcpassword]
-TESTNET_DIR = File.join(File.dirname(__FILE__), 'testnet')
+
+BitcoinTestnet.configure_rspec!
+BitcoinTestnet.dir = File.join(File.dirname(__FILE__), 'testnet')
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.before(:suite) do
-    # Ensure that the testnet is running.
-    bitcoin_processes = `ps -ef | grep bitcoin | grep -v grep`.split("\n")
-    unless bitcoin_processes.size == 2
-      fail "Please make sure the testnet has started. Run `cd #{TESTNET_DIR} && make start`"
-    end
-  end
 
   config.before(:each) do
-    system("cd #{TESTNET_DIR} && make clean > /dev/null")
-
-    # Do not wait for confirmations in tests -- too long!
     BitWallet.config.min_conf = 0
   end
 end
