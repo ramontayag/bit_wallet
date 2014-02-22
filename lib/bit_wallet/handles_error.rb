a3a085e056446a890fddd97ff311d17cec24add5
Ace Subido and Ramon Tayag <dev+ace+ramon@aelogica.com>
2014-02-22 13:57:53 +0800

    Raise error if message is unknown

--- lib/bit_wallet/handles_error.rb
+++ lib/bit_wallet/handles_error.rb
@@ -4,6 +4,8 @@ module BitWallet
     def self.from(message)
       error = if message.include?("-6")
         InsufficientFunds.new("cannot send an amount more than what this account has")
+      else
+        ArgumentError.new("unknown error: #{message}")
       end
       fail error if error
     end
