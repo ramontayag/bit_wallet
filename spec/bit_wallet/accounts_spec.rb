02761100ed8984cceb368c05bca5124a4087cb22
Ramon Tayag <ramon.tayag@gmail.com>
2014-03-04 21:47:52 +0800

    Wrap test in module for shorter constant names

--- spec/bit_wallet/accounts_spec.rb
+++ spec/bit_wallet/accounts_spec.rb
@@ -1,45 +1,47 @@
 require 'spec_helper'
 
-describe BitWallet::Accounts, vcr: {record: :once} do
+module BitWallet
+  describe Accounts, vcr: {record: :once} do
 
-  let(:wallet) do
-    build(:wallet)
-  end
-
-  subject do
-    BitWallet::Accounts.new(wallet)
-  end
-
-  its(:wallet) { should == wallet }
-
-  describe '#new' do
-    it 'should create a new BitWallet::Account with a default address' do
-      account = wallet.accounts.new('accountname')
-      account.should be_kind_of(BitWallet::Account)
-      account.addresses.count.should == 1
+    let(:wallet) do
+      build(:wallet)
     end
-  end
 
-  describe '#includes_account_name?(account)' do
-    it 'should return true if the array includes the account' do
-      account = subject.new('accountname')
-      subject.includes_account_name?('accountname').should == true
+    subject do
+      Accounts.new(wallet)
     end
-  end
 
-  describe '.with_balance' do
-    it 'should return accounts with a balance > 0' do
-      default_account = subject.new('')
-      account_1 = subject.new('nomoney')
-      account_2 = subject.new('moneyd')
+    its(:wallet) { should == wallet }
 
-      default_account.send_amount 10, to: account_2.addresses.first
-
-      accounts_with_balance = subject.with_balance
-      accounts_with_balance.should include(default_account)
-      accounts_with_balance.should_not include(account_1)
-      accounts_with_balance.should include(account_2)
+    describe '#new' do
+      it 'should create a new BitWallet::Account with a default address' do
+        account = wallet.accounts.new('accountname')
+        account.should be_kind_of(Account)
+        account.addresses.count.should == 1
+      end
     end
-  end
 
+    describe '#includes_account_name?(account)' do
+      it 'should return true if the array includes the account' do
+        account = subject.new('accountname')
+        subject.includes_account_name?('accountname').should == true
+      end
+    end
+
+    describe '.with_balance' do
+      it 'should return accounts with a balance > 0' do
+        default_account = subject.new('')
+        account_1 = subject.new('nomoney')
+        account_2 = subject.new('moneyd')
+
+        default_account.send_amount 10, to: account_2.addresses.first
+
+        accounts_with_balance = subject.with_balance
+        accounts_with_balance.should include(default_account)
+        accounts_with_balance.should_not include(account_1)
+        accounts_with_balance.should include(account_2)
+      end
+    end
+
+  end
 end
--- spec/bit_wallet/wallet_spec.rb
+++ spec/bit_wallet/wallet_spec.rb
@@ -1,63 +1,65 @@
 require 'spec_helper'
 
-describe BitWallet::Wallet, vcr: true do
+module BitWallet
+  describe Wallet, vcr: true do
 
-  describe '#accounts' do
-    it 'should return array of BitWallet::Accounts' do
-      wallet = build(:wallet)
-      wallet.accounts.should be_kind_of(BitWallet::Accounts)
+    describe '#accounts' do
+      it 'should return array of BitWallet::Accounts' do
+        wallet = build(:wallet)
+        wallet.accounts.should be_kind_of(Accounts)
+      end
     end
-  end
 
-  describe '#recent_transactions' do
-    it 'should return the most recent transactions of all accounts defaulting to 10 transactions' do
-      wallet = build(:wallet)
-      default_account = wallet.accounts.new('')
-      account_1 = wallet.accounts.new('1')
+    describe '#recent_transactions' do
+      it 'should return the most recent transactions of all accounts defaulting to 10 transactions' do
+        wallet = build(:wallet)
+        default_account = wallet.accounts.new('')
+        account_1 = wallet.accounts.new('1')
 
-      1.upto(11).each do |n|
-        default_account.send_amount n, to: account_1.addresses.first
+        1.upto(11).each do |n|
+          default_account.send_amount n, to: account_1.addresses.first
+        end
+
+        wallet.recent_transactions.size.should == 10
       end
 
-      wallet.recent_transactions.size.should == 10
-    end
+      it 'should allow overriding of the transaction limit' do
+        wallet = build(:wallet)
+        default_account = wallet.accounts.new('')
+        account_1 = wallet.accounts.new('1')
 
-    it 'should allow overriding of the transaction limit' do
-      wallet = build(:wallet)
-      default_account = wallet.accounts.new('')
-      account_1 = wallet.accounts.new('1')
+        1.upto(11).each do |n|
+          default_account.send_amount n, to: account_1.addresses.first
+        end
 
-      1.upto(11).each do |n|
-        default_account.send_amount n, to: account_1.addresses.first
+        wallet.recent_transactions(limit: 5).size.should == 5
       end
-
-      wallet.recent_transactions(limit: 5).size.should == 5
     end
-  end
 
-  describe '#move' do
-    it 'should move funds from one account to another' do
-      wallet = build(:wallet)
-      default_account = wallet.accounts.new('')
-      account_1 = wallet.accounts.new('1')
-      wallet.move '', '1', 1.1
-      wallet.move default_account, account_1, 0.5
-      account_1.balance.should == 1.6
+    describe '#move' do
+      it 'should move funds from one account to another' do
+        wallet = build(:wallet)
+        default_account = wallet.accounts.new('')
+        account_1 = wallet.accounts.new('1')
+        wallet.move '', '1', 1.1
+        wallet.move default_account, account_1, 0.5
+        account_1.balance.should == 1.6
+      end
     end
-  end
 
-  describe "#client" do
-    it "returns the client created by InstantiatesBitcoinClient" do
-      client = double
-      config = {some: "config"}
-      allow(BitWallet::InstantiatesBitcoinClient).
-        to receive(:execute).with(config).
-        and_return(client)
+    describe "#client" do
+      it "returns the client created by InstantiatesBitcoinClient" do
+        client = double
+        config = {some: "config"}
+        allow(InstantiatesBitcoinClient).
+          to receive(:execute).with(config).
+          and_return(client)
 
-      wallet = described_class.new(config)
+        wallet = described_class.new(config)
 
-      expect(wallet.client).to eq(client)
+        expect(wallet.client).to eq(client)
+      end
     end
-  end
 
+  end
 end
